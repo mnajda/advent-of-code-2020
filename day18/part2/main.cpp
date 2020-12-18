ac3142ff6443aedd063bc43c46f76e2dacf05eae
Mateusz Najda <mateusznajda@gmail.com>
2020-12-18 12:28:30 +0100

    cleanup

--- day18/part2/main.cpp
+++ day18/part2/main.cpp
@@ -160,7 +160,7 @@ int main(int argc, char** argv)
 {
     if (argc < 2)
     {
-        throw std::runtime_error("Not filename provided");
+        throw std::runtime_error("No filename provided");
     }
 
     const auto input = load_file(argv[1]);
